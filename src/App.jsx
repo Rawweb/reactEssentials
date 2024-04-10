eef5bde5028e2c819dd6e88a9039b7491d38126d
Rawfile Web <rawfile.webdev@gmail.com>
2024-04-10 21:32:36 +0100

    Completed the REACT Essentials coding and Tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+import {useState} from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton  from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
+    const [selectedTopic, setSelectedTopic] = useState();
+
+    function handleSelect(selectedButton) {
+        // selectedButton should be a string for one of these => 'componenets', 'jsx', 'props', and 'state'
+        setSelectedTopic(selectedButton);
+        // console.log(selectedTopic);
+    }
+
+    // THIRD METHOD
+    let tabContent = <p>Please select a topic</p>;
+
+    if (selectedTopic) {
+        tabContent = (
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                    <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+            </div>
+        );
+    }
+
     return (
         <div>
             <Header></Header>
@@ -21,6 +48,49 @@ function App() {
                         <CoreConcept {...CORE_CONCEPTS[3]} />
                     </ul>
                 </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton onSelect={() => handleSelect("components")}>
+                            Components
+                        </TabButton>
+                        <TabButton onSelect={() => handleSelect("jsx")}>
+                            JSX
+                        </TabButton>
+                        <TabButton onSelect={() => handleSelect("props")}>
+                            Props
+                        </TabButton>
+                        <TabButton onSelect={() => handleSelect("state")}>
+                            State
+                        </TabButton>
+                    </menu>
+                    {/* FIRST METHOD */}
+                    {/* {!selectedTopic ? (
+                        <p>Please select a topic.</p>
+                    ) : (
+                        <div id="tab-content">
+                            <h3>{EXAMPLES[selectedTopic].title}</h3>
+                            <p>{EXAMPLES[selectedTopic].description}</p>
+                            <pre>
+                                <code>{EXAMPLES[selectedTopic].code}</code>
+                            </pre>
+                        </div>
+                    )} */}
+
+                    {/* SECOND METHOD */}
+                    {/* {!selectedTopic && <p>Please select a topic</p>}
+                    {selectedTopic && (
+                        <div id="tab-content">
+                            <h3>{EXAMPLES[selectedTopic].title}</h3>
+                            <p>{EXAMPLES[selectedTopic].description}</p>
+                            <pre>
+                                <code>{EXAMPLES[selectedTopic].code}</code>
+                            </pre>
+                        </div>
+                    )} */}
+                    {tabContent}
+                </section>
+                
             </main>
         </div>
     );
